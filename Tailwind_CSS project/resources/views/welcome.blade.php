deded412011fa5e19cf29fd75bd5522a062e2f5d
safiullah karimi <153161008+Safiullah751@users.noreply.github.com>
2024-10-29 17:16:13 +0430

    Testimonial
    Testimonial section added after features section

--- Tailwind_CSS project/resources/views/welcome.blade.php
+++ Tailwind_CSS project/resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
 
         {{-- Button --}}
         <a href=""
-            class="hidden xl:block p-2 px-10 text-white bg-orange-500 rounded-full pt-3 hover:bg-black delay-100 ease-in-out">Get
+            class="hidden xl:block p-2 px-10 text-white bg-red-400 rounded-full pt-3 hover:bg-black delay-100 ease-in-out">Get
             Started</a>
     </div>
 </nav>
@@ -58,7 +58,7 @@
             </p>
             <div class="flex justify-center md:justify-start">
                 <a href=""
-                    class="p-2 px-10 text-white bg-orange-500 rounded-full pt-3 hover:bg-black delay-100 ease-in-out">Get
+                    class="p-2 px-10 text-white bg-red-400 rounded-full pt-3 hover:bg-black delay-100 ease-in-out">Get
                     Started</a>
             </div>
         </div>
@@ -174,12 +174,63 @@
 </section>
 
   {{-- TestTimonails --}}
-         <section id="testtimonails">
-            <div class=" mx-auto max-w-6xl text-center px-5 mt-32">
-                <h2 class="text-center px-12 text-4xl font-bold" >
-                     What's Different About Manage?
-                </h2>
-            </div>
-         </section>
+  <section id="testimonials">
+    <!-- Container to heading and testm blocks -->
+    <div class="max-w-6xl px-5 mx-auto mt-32 text-center">
+      <!-- Heading -->
+      <h2 class="text-4xl font-bold text-center">
+        What's Different About Manage?
+      </h2>
+      <!-- Testimonials Container -->
+      <div class="flex flex-col mt-24 md:flex-row md:space-x-6">
+        <!-- Testimonial 1 -->
+        <div
+          class="flex flex-col items-center p-6 space-y-6 rounded-lg bg-slate-100 md:w-1/3"
+        >
+          <img src="img/avatar-anisha.png" class="w-16 -mt-14" alt="" />
+          <h5 class="text-lg font-bold">Anisha Li</h5>
+          <p class="text-sm text-darkGrayishBlue">
+            “Manage has supercharged our team’s workflow. The ability to
+            maintain visibility on larger milestones at all times keeps
+            everyone motivated.”
+          </p>
+        </div>
+
+        <!-- Testimonial 2 -->
+        <div
+          class="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-slate-100 md:flex md:w-1/3"
+        >
+          <img src="img/avatar-ali.png" class="w-16 -mt-14" alt="" />
+          <h5 class="text-lg font-bold">Ali Bravo</h5>
+          <p class="text-sm text-darkGrayishBlue">
+            “We have been able to cancel so many other subscriptions since
+            using Manage. There is no more cross-channel confusion and
+            everyone is much more focused.”
+          </p>
+        </div>
+
+        <!-- Testimonial 3 -->
+        <div
+          class="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-slate-100 md:flex md:w-1/3"
+        >
+          <img src="img/avatar-richard.png" class="w-16 -mt-14" alt="" />
+          <h5 class="text-lg font-bold">Richard Watts</h5>
+          <p class="text-sm text-darkGrayishBlue">
+            “Manage has supercharged our team’s workflow. The ability to
+            maintain visibility on larger milestones at all times keeps
+            everyone motivated.”
+          </p>
+        </div>
+      </div>
+      <!-- Button -->
+      <div class="my-16">
+        <a
+          href="#"
+          class="p-3 px-6 pt-2 text-white bg-red-400 rounded-full baseline hover:bg-brightRedLight"
+          >Get Started</a
+        >
+      </div>
+    </div>
+  </section>
 </body>
 </html>
